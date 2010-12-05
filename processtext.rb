017011aff043777c97410df0fc3fd34a1efe7993
Stian Haklev <shaklev@gmail.com>
2010-12-05 13:05:28 -0500

    more description

--- processtext.rb
+++ processtext.rb
@@ -1,12 +1,18 @@
 # this is an experiment in processing tweet texts
 
+# produces a hash of all tags, with their description
+# ie turns this
+# ttt_test #ttt_test2 #damage South tower collapsed #loc 43.634087,-79.341888  #status Dragon attacking castle.
+# into this
+# {"loc"=>"43.634087,-79.341888", "damage"=>"South tower collapsed", "status"=>"Dragon attacking castle.", "ttt_test"=>"", "ttt_test2"=>""}
 def process(line)
-  tags = Hash.new
+  tags = Hash.new  
   line.split("#").each do |tag| 
     next if tag.size == 0
-    tag, desc = tag.split(" ")
-    tags[tag] = desc
+    tag, *desc = tag.split(" ")
+    tags[tag] = desc.join(" ")
   end
+  puts line
   p tags
 end
 
