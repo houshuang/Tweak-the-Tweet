ae2ea076537686bc34ce2b957fc166ddcf310879
Stian Haklev <shaklev@gmail.com>
2010-12-05 11:57:41 -0500

    intro to how procdump works

--- processdump.rb
+++ processdump.rb
@@ -1,4 +1,6 @@
-# reads tweets from the queue, parses them, and outputs them
+# reads tweets from the queue, parses them, and outputs them. it starts a loop reading each
+# file, and when there are no new files, it sleeps for a second and checks again, for ever.
+# this way it can keep running and will wake up when tweets arrive
 
 require 'rubygems'
 require 'tweetstream'
@@ -6,8 +8,11 @@ require 'filelib'
 require 'library'
 
 r = Filelib::Reader.new
-while (r.morechunks?)
-  r.readchunk.each do |tweet|
-    puts tweet.user.name + ": " + tweet.text
+while(1)
+  while (r.morechunks?)
+    r.readchunk.each do |tweet|
+      puts tweet.user.name + ": " + tweet.text
+    end
   end
+  sleep(1)
 end
