3d8bf0930f68ec7657dae593d7b472c82cd721da
Stian Haklev <shaklev@gmail.com>
2010-12-04 19:31:58 -0500

    added state-independent position counter

--- library.rb
+++ library.rb
@@ -7,3 +7,21 @@ def location(adr)
   Geokit::Geocoders::MultiGeocoder.geocode(adr + ", #{@conf.city}")
 end
 
+class Position          # simply keeps the position, by writing to an external file. state independent.
+  attr_reader :pos
+
+  def initialize
+    if File.exists?("position")
+      @pos = File.read("position").to_i
+    else
+      @pos = 0
+    end
+    @file = File.open('position','w')
+  end
+
+  def pos=(newpos)
+    @pos=newpos
+    @file.seek(0)
+    @file << @pos.to_s
+  end  
+end
